4c3b4976112ea2933021ef1a3b6486f1688b0212
Panu Ausavasereelert <panu@panu.in.th>
2013-12-26 21:45:38 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flipcard::Application.config.secret_key_base = 'fd73fc1653de54e60a66ee82ffbc3e106da9cd1bc566f934e6c8ead3b8c37b9f6625b2fb3b1b783e7d397975b283f50603969d665e13376098d17f66e3109f54'
